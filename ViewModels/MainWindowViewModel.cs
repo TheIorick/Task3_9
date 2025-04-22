1ab01ad711cbae48e6ddfac4ef046f75f41771a3
The_Iorick <kotolevskij.vlad@mail.ru>
2025-04-22 23:54:37 +0300

    init

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Task3_9.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Task3_9.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
